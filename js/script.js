13c0145c0bc21c15dd39bfcc2109d33fd1d40afe
Jai Prakash Patel <prakashjaipatel1@gmail.com>
2023-09-09 16:59:05 +0530

    Create a new repository

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// function navbar(){
+
+//     if(document.getElementById("line2").style.display){
+//         document.getElementById("line2").style.display="block";
+//     }
+//     else{
+//         document.getElementById("line2").style.display="none";
+//     }
+// }
+
